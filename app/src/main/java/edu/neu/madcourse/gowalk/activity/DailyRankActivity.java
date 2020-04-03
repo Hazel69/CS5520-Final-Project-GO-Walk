c5b860fcb16455bbdb86a58ab5d85b1aef89c033
xiaojinhe <irisxjhe@gmail.com>
2020-04-03 14:27:29 -0700

    added share to daily rank

--- app/src/main/java/edu/neu/madcourse/gowalk/activity/DailyRankActivity.java
+++ app/src/main/java/edu/neu/madcourse/gowalk/activity/DailyRankActivity.java
@@ -2,14 +2,21 @@ package edu.neu.madcourse.gowalk.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import edu.neu.madcourse.gowalk.R;
+import edu.neu.madcourse.gowalk.fragment.ShareFragment;
+import edu.neu.madcourse.gowalk.util.FCMUtil;
+import edu.neu.madcourse.gowalk.util.SharedPreferencesUtil;
 import edu.neu.madcourse.gowalk.viewmodel.DailyRankViewModel;
 
 public class DailyRankActivity extends AppCompatActivity {
@@ -22,6 +29,9 @@ public class DailyRankActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_rank);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         recyclerView = findViewById(R.id.dailyRankRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setVisibility(View.VISIBLE);
@@ -30,6 +40,28 @@ public class DailyRankActivity extends AppCompatActivity {
         dailyRankViewModel.getDailyRankListLiveData().observe(this, mDailySteps -> {
             recyclerView.setAdapter(new DailyRankingAdapter(mDailySteps));
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.homepage_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        if (item.getItemId() == R.id.share_menu_item) {
+            this.showShareFragment();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showShareFragment() {
+        ShareFragment shareFragment = new ShareFragment();
+        shareFragment.show(getSupportFragmentManager(), "shareFragment");
     }
 
     public void directToDailyRanking(View view) {
@@ -43,6 +75,20 @@ public class DailyRankActivity extends AppCompatActivity {
     public void directToSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    public void sendMessageSteps(MenuItem item) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //TODO: need to use username
+                String userId = SharedPreferencesUtil.getUserId(getApplicationContext());
+                String msgTitle = String.format(getString(R.string.send_steps_title), userId);
+                //TODO: use actual steps
+                String msgBody = String.format(getString(R.string.send_steps_body), userId, 10000);
+                FCMUtil.sendMessageToTopic(msgTitle, msgBody, getString(R.string.steps_topic));
+            }
+        }).start();
     }
 
 }
